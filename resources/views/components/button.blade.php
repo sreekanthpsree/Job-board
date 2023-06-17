17697b2ef9d8ed13664d59e7e20445fcb422077c
Sreekanth <116191030+sreekanthpsree@users.noreply.github.com>
2023-06-17 19:26:36 +0530

    added Feature

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+<button
+    {{ $attributes->class(['border rounded-md border-slate-300 px-2.5 py-1.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-slate-500']) }}>
+    {{ $slot }}
+</button>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel Job board</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
-<body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 via-sky-300 to-emerald-200 text-slate-700">
     {{ $slot }}
 </body>
 
